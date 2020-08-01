1d5242f91f06121a73308d3068a8a0cd1f42c45a
ShubbhamJain <shubham.jain368@gmail.com>
2020-08-02 04:33:19 +0530

    commit changes

--- server.js
+++ server.js
@@ -24,14 +24,15 @@ app.use(bodyParser.json());
 
 app.use(helmet());
 
+router.get("/favicon.ico", (req, res) => res.status(200));
 app.use("/api/items", items);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  // });
 }
 
 app.listen(PORT, () => {
